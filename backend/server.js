1e3e3a131819c94e1df6c1253f57bc2852c70a0c
willian.rochagit config --global user.name <willian.rocha@viaflow.com.br>
2020-05-12 14:12:29 -0300

    final

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
+require('dotenv');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const PORT = 3333 || process.env.PORT
 
 const routes = require('./routes');
 
@@ -9,6 +11,5 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333 || process.env.PORT);
+app.listen(PORT);
 
- 
